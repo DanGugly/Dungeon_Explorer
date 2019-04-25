16a73b93c2489de2ed9521386fa657019673779c
DanGugly <you@example.com>
2019-04-24 20:23:18 -0400

    Added You won and You lost gameOver screens

--- src/com/dangugly/game/states/GameOverState.java
+++ src/com/dangugly/game/states/GameOverState.java
@@ -22,11 +22,14 @@ public class GameOverState extends GameState {
     private int alpha;
     private int ticks;
 
+    boolean won = false;
+
     private final int FADE_IN = 240;
 
     public GameOverState(GameStateManager gsm, boolean win) {
         super(gsm);
 
+        won = win;
         if(win){
             end = Sound.getClip("win");
         }else {
@@ -67,12 +70,18 @@ public class GameOverState extends GameState {
         g.fillRect(0, 0, GamePanel.WIDTH +1280, GamePanel.height);
         g.drawImage(logo, 0, 0, GamePanel.WIDTH +1280 , GamePanel.height, null);
         g.drawImage(score, GamePanel.WIDTH +340, GamePanel.height -450, GamePanel.WIDTH +580 , GamePanel.height -550, null);
-
-        //Sprite.drawArray(g, gsm.font, "     "+gsm.life, new Vector2f(480,10), 48, 24);
+        Sprite.drawArray(g, gsm.font, " GAME OVER", new Vector2f(480,55), 44, 24);
+        if (won){
+            Sprite.drawArray(g, gsm.font, "  YOU WON", new Vector2f(480,135), 44, 24);
+        } else {
+            Sprite.drawArray(g, gsm.font, " YOU LOST", new Vector2f(480,135), 44, 24);
+        }
+        Sprite.drawArray(g, gsm.font, "FINAL SCORE", new Vector2f(480,215), 42, 24);
         Sprite.drawArray(g, gsm.font, ""+gsm.life, new Vector2f(750,275), 48, 24);
         Sprite.drawArray(g, gsm.font, "  "+gsm.kills , new Vector2f(750,315), 48, 24);
         Sprite.drawArray(g, gsm.font,"  "+gsm.saved , new Vector2f(750,355), 48, 24);
         Sprite.drawArray(g, gsm.font,"  "+gsm.deaths , new Vector2f(750,395), 48, 24);
+        Sprite.drawArray(g, gsm.font,"PRESS ENTER FOR MENU AND ESC TO EXIT" , new Vector2f(230,480), 36, 24);
     }
 
     public void init() {
